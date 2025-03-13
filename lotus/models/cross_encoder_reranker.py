a3b8c279d79588c251216923adb4df162899eae4
vincent-4 <vincentzhongy+githubvincent4@gmail.com>
2025-03-13 17:03:06 -0400

    Defer initialization so we have quicker import (should be <<< 1 second).

--- lotus/models/cross_encoder_reranker.py
+++ lotus/models/cross_encoder_reranker.py
@@ -13,6 +13,14 @@ class CrossEncoderReranker(Reranker):
         max_batch_size (int): The maximum batch size to use for the model.
     """
 
+    # Use lazy init instead of eager because otherwise we have very slow import times.
+    @property
+    def model(self):
+        if self._model is None:
+            self._model = CrossEncoder(model=self.model, device=self.device) # type: ignore # CrossEncoder has wrong type stubs
+        return self._model
+
+
     def __init__(
         self,
         model: str = "mixedbread-ai/mxbai-rerank-large-v1",
@@ -20,7 +28,7 @@ class CrossEncoderReranker(Reranker):
         max_batch_size: int = 64,
     ):
         self.max_batch_size: int = max_batch_size
-        self.model = CrossEncoder(model, device=device)  # type: ignore # CrossEncoder has wrong type stubs
+        self.model = None  # type: ignore # CrossEncoder has wrong type stubs. Also defer initialization.
 
     def __call__(self, query: str, docs: list[str], K: int) -> RerankerOutput:
         results = self.model.rank(query, docs, top_k=K, batch_size=self.max_batch_size, show_progress_bar=False)
